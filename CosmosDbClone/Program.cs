51bc472a5afcfd87642fa663c5f71cf0163569dc
castroal <alberto.castro@live.it>
2025-06-25 09:14:40 +0200

    Added support for multi account

--- CosmosDbClone/Program.cs
+++ CosmosDbClone/Program.cs
@@ -16,15 +16,19 @@ namespace CosmosDbClone
             IConfigurationRoot configuration = builder.Build();
 
             var sourceConnectionString = configuration.GetConnectionString("Source");
+            var targetConnectionString = configuration.GetConnectionString("Target") ?? sourceConnectionString;
             var sourceDatabaseName = configuration.GetValue<string>("SourceDbName");
             var targetDatabaseName = configuration.GetValue<string>("TargetDbName");
 
-            using var cosmosClient = new CosmosClient(sourceConnectionString);
+            using var sourceClient = new CosmosClient(sourceConnectionString);
+            using var targetClient = targetConnectionString == sourceConnectionString 
+                ? sourceClient 
+                : new CosmosClient(targetConnectionString);
 
-            var sDatabase = cosmosClient.GetDatabase(sourceDatabaseName);
-            var tDatabase = (await cosmosClient.CreateDatabaseIfNotExistsAsync(targetDatabaseName)).Database;
+            var sourceDatabase = sourceClient.GetDatabase(sourceDatabaseName);
+            var targetDatabase = (await targetClient.CreateDatabaseIfNotExistsAsync(targetDatabaseName)).Database;
 
-            AnsiConsole.MarkupLine($"Copying [green]{sourceDatabaseName}[/] to [red]{targetDatabaseName}[/] ...");
+            AnsiConsole.MarkupLine($"Copying [green]{sourceDatabaseName} {sourceClient.Endpoint.Host}[/] to [red]{targetDatabaseName} {targetClient.Endpoint.Host}[/] ...");
 
             var progress = AnsiConsole.Progress();
             progress.RefreshRate = TimeSpan.FromSeconds(0.5);
@@ -35,17 +39,17 @@ namespace CosmosDbClone
                 .StartAsync(async ctx =>
                 {
                     var copyTasks = new List<Task>();
-                    var sContainersIterator = sDatabase.GetContainerQueryIterator<ContainerProperties>();
+                    var sourceContainersIterator = sourceDatabase.GetContainerQueryIterator<ContainerProperties>();
                     do
                     {
-                        var containers = await sContainersIterator.ReadNextAsync();
-                        foreach (var sContainerProperties in containers)
+                        var containers = await sourceContainersIterator.ReadNextAsync();
+                        foreach (var sourceContainerProperties in containers)
                         {
-                            var progressTask = ctx.AddTask($"{sContainerProperties.Id}");
-                            copyTasks.Add(CloneContainerAsync(sDatabase, tDatabase, sContainerProperties, progressTask));
+                            var progressTask = ctx.AddTask($"{sourceContainerProperties.Id}");
+                            copyTasks.Add(CloneContainerAsync(sourceDatabase, targetDatabase, sourceContainerProperties, progressTask));
                         }
                     }
-                    while (sContainersIterator.HasMoreResults);
+                    while (sourceContainersIterator.HasMoreResults);
 
                     await Task.WhenAll(copyTasks);
                 });
@@ -53,31 +57,31 @@ namespace CosmosDbClone
             AnsiConsole.MarkupLine("[green]Done![/]");
         }
 
-        private static async Task CloneContainerAsync(Database sDatabase, Database tDatabase, ContainerProperties sContainerProperties, ProgressTask progressTask)
+        private static async Task CloneContainerAsync(Database sourceDatabase, Database targetDatabase, ContainerProperties sContainerProperties, ProgressTask progressTask)
         {
             await maxParallelism.WaitAsync();
             try
             {
                 progressTask.StartTask();
-                var sContainer = sDatabase.GetContainer(sContainerProperties.Id);
-                var tContainerProperties = new ContainerProperties
+                var sourceContainer = sourceDatabase.GetContainer(sContainerProperties.Id);
+                var targetContainerProperties = new ContainerProperties
                 {
                     Id = sContainerProperties.Id,
                     PartitionKeyPath = sContainerProperties.PartitionKeyPath,
                     UniqueKeyPolicy = sContainerProperties.UniqueKeyPolicy,
                 };
-                var tContainer = (await tDatabase.CreateContainerIfNotExistsAsync(tContainerProperties)).Container;
+                var targetContainer = (await targetDatabase.CreateContainerIfNotExistsAsync(targetContainerProperties)).Container;
 
-                progressTask.MaxValue = (await sContainer.GetItemQueryIterator<int>("SELECT VALUE COUNT(1) FROM c").ReadNextAsync()).Resource.First();
+                progressTask.MaxValue = (await sourceContainer.GetItemQueryIterator<int>("SELECT VALUE COUNT(1) FROM c").ReadNextAsync()).Resource.First();
 
-                var sIterator = sContainer.GetItemQueryIterator<dynamic>(requestOptions: new QueryRequestOptions { MaxItemCount = 500 });
+                var sourceIterator = sourceContainer.GetItemQueryIterator<dynamic>(requestOptions: new QueryRequestOptions { MaxItemCount = 500 });
                 do
                 {
-                    var sItems = await sIterator.ReadNextAsync();
-                    await Parallel.ForEachAsync(sItems, async (sItem, _) => await tContainer.CreateItemAsync(sItem));
-                    progressTask.Increment(sItems.Count);
+                    var sourceItems = await sourceIterator.ReadNextAsync();
+                    await Parallel.ForEachAsync(sourceItems, async (sourceItem, _) => await targetContainer.CreateItemAsync(sourceItem));
+                    progressTask.Increment(sourceItems.Count);
                 }
-                while (sIterator.HasMoreResults);
+                while (sourceIterator.HasMoreResults);
                 progressTask.StopTask();
             }
             finally
